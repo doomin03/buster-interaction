e8c7da334980a6f5b5643007512d951d7e799c7b
jung3 <jung3232aa@gmail.com>
2025-04-28 02:03:45 +0900

    세팅

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container-main">
+    <Intro></Intro>
+  </div>
+</template>
+
+<script>
+  import Intro from "@/components/Intro.vue";
+
+  export default {
+    name: "App",
+    components: {
+      Intro,
+    }
+  }
+</script>
---
+++ src/components/Intro.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="container-content section-gray">
+    <div class="header">
+      <div class="header-logo">
+        <div class="icon"></div>
+      </div>
+      <div class="header-nav">
+        <div class="nav-item">About</div>
+        <div class="nav-item">Vision</div>
+        <div class="nav-item">Features</div>
+        <div class="nav-item">Values</div>
+        <div class="nav-item">FAQ</div>
+        <button class="download-btn">앱 다운로드</button>
+      </div>
+    </div>
+
+    <div class="main-box">
+      <div class="box-main-title">
+        대중교통, <br>
+        더 빠르고 똑똑하게
+      </div>
+      <div class="box-sub-title">
+        매일의 이동이 더 간편하고 가벼워집니다.
+      </div>
+      <div class="download-box">앱 다운로드 하기 <button class="download-btn"><div class="icon"></div> </button></div>
+    </div>
+
+    <div class="mt192 ta-c">
+      <div class="fw700 fs48 c1A1A1A">버스터와 함께 즐거운 출근길</div>
+      <div class="fw400 fs18 c383838 mt14">바쁜 아침, 더 빠른 길을 찾느라 헤매지 마세요.</div>
+      <div class="fw400 fs18 c383838 mt4">버스터는 매일의 출근길을 더 똑똑하고 편리하게 만듭니다.</div>
+    </div>
+
+
+  </div>
+</template>
+<script>
+export default {
+  name: "Intro",
+  data(){
+    return {}
+  },
+  mounted() {},
+  unmounted() {},
+  methods: {},
+}
+</script>
